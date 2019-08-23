d0d1590fb75a122a5070d2410e6e7e56b960c951
Andy Holt <andyholt85@gmail.com>
2019-08-23 15:35:48 -0600

    mocked up static pages and configured database login environment variables

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  # Generates a page's full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "SkipTraceBook"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+  
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @base_title = "SkipTraceBook"  
+  end
+  
+  test "should get home" do
+    get root_url
+    assert_response :success
+    assert_select "title", "#{@base_title}"
+  end
 end
