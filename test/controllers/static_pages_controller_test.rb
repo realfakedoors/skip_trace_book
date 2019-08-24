acbc9a7cb43a5efc2940d99b8960fa08a13ceb9c
Andy Holt <andyholt85@gmail.com>
2019-08-24 15:36:33 -0600

    added a few tests for StaticPagesController

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,4 +11,22 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "#{@base_title}"
   end
+  
+  test "should get about" do
+    get about_url
+    assert_response :success
+    assert_select "title", "About Us | #{@base_title}"
+  end
+  
+  test "should get help" do
+    get help_url
+    assert_response :success
+    assert_select "title", "Help | #{@base_title}"
+  end
+  
+  test "should get contact" do
+    get contact_url
+    assert_response :success
+    assert_select "title", "Drop Us A Line | #{@base_title}"
+  end
 end
