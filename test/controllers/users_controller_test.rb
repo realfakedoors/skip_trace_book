727913a6b627fd431f70d1771ab10f4eba3e6168
Andy Holt <andyholt85@gmail.com>
2019-09-05 12:29:10 -0600

    added user validations for name and email

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @user = users(:one)
+    @user = users(:RealPerson)
   end
 
   test "should get index" do
@@ -13,14 +13,6 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get signup_url
     assert_response :success
-  end
-
-  test "should create user" do
-    assert_difference('User.count') do
-      post users_url, params: { user: { email: @user.email, name: @user.name } }
-    end
-
-    assert_redirected_to user_url(User.last)
   end
 
   test "should show user" do
